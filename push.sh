b85c3485b90bd30b4f08e4cc5c52febdcba7cbf3
waraunika <waraunika2956@gmail.com>
2025-11-22 13:19:23 +0545

    Started on Ch-1 for Communication System
    Completed Chapter 1, and created file for chapter 2 for CS, based on PYQs

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-message1="Started on Ch-1 for Communication System"
-message2="Completed Chapter 1, and created file for chapter 2 for CS, based on PYQs"
-
-git add .
-git commit -m "$message1" -m "$message2"
-git push -u origin main
